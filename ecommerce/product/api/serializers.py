424fd3b770c73a4ba59def4c026e7c6674212a0e
byrchard <batuhanrecep06@hotmail.com>
2023-12-05 21:38:42 +0300

    .

--- ecommerce/product/api/serializers.py
+++ ecommerce/product/api/serializers.py
@@ -1,7 +1,6 @@
 from pyexpat import model
 from rest_framework.serializers import ModelSerializer
 from ..models import Product, Category, OrderWithoutMembership
-from django.db import transaction
 
 class ProductSerializer(ModelSerializer):
     class Meta:
@@ -18,6 +17,6 @@ class CategorySerializer(ModelSerializer):
 class OrderWithoutMembershipSerializer(ModelSerializer):
     class Meta:
         model = OrderWithoutMembership
-        fields = ('product', 'quantity','firstname','lastname','phone_number','zip_code', 'city', 'country', 'street', 'state','is_active')
+        fields = ('firstname','lastname','phone_number','zip_code', 'city', 'country', 'street', 'state','is_active')
 
-    
+
