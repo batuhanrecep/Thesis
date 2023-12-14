a562bbf8a38b469fbe276ff38d198d144d04f6fe
byrchard <batuhanrecep06@hotmail.com>
2023-12-15 01:24:07 +0300

    category name currently appears in data

--- ecommerce/product/api/serializers.py
+++ ecommerce/product/api/serializers.py
@@ -2,13 +2,17 @@ from pyexpat import model
 from rest_framework.serializers import ModelSerializer
 from ..models import Product, Category
 
-class ProductSerializer(ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('id', 'title', 'image', 'stock', 'description', 'is_active', 'is_home', 'slug', 'categories','regular_price', 'discount_price','updated_at','created_at')
+
 
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = ('id','name','slug')   
+
+class ProductSerializer(ModelSerializer):
+    categories = CategorySerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Product
+        fields = ('id', 'title', 'image', 'stock', 'description', 'is_active', 'is_home', 'slug', 'categories','regular_price', 'discount_price','updated_at','created_at')
