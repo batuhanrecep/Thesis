e7e974eae7dc13587a567e32b061d9e6d9aa2958
byrchard <batuhanrecep06@hotmail.com>
2023-12-14 17:02:07 +0300

    auth url changes

--- ecommerce/authentication/api/urls.py
+++ ecommerce/authentication/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import views
 
-
+#localhost/api/auth/...
 urlpatterns = [
     path('create-user-customer/', views.CreateUserCustomer.as_view(), name='create-user-customer'),
 ]
