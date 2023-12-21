b223047e5d78bed18dfcb6fec7e704a518a0ce6b
byrchard <batuhanrecep06@hotmail.com>
2023-12-21 13:33:20 +0300

    jwt urls added

--- ecommerce/authentication/api/urls.py
+++ ecommerce/authentication/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 #localhost/api/auth/...
@@ -9,6 +9,9 @@ from . import views
 #http://127.0.0.1:8000/api/auth/register/seller/
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+    path('token/verify/', TokenVerifyView.as_view()),
     path('register/customer/', views.customer_register, name='register'),
     path('register/seller/', views.seller_register, name='register'),
     path('login/', views.login, name='login'),
